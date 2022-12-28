0e70cb82d70ceba888048b48562e3b1549314dee
BisiDavi <oludavidconnect@gmail.com>
2022-12-28 18:44:29 +0100

    pages updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,66 +1,72 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link
-                        rel='preconnect'
-                        href='https://fonts.googleapis.com'
-                    />
-                    <link
-                        rel='preconnect'
-                        href='https://fonts.gstatic.com'
-                        crossOrigin='true'
-                    />
-                    <link
-                        href='https://fonts.googleapis.com/css2?family=Raleway&display=swap'
-                        rel='stylesheet'
-                    />
-                    <link
-                        rel='stylesheet'
-                        href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'
-                    />
-                    <link
-                        rel='apple-touch-icon'
-                        sizes='180x180'
-                        href='/apple-touch-icon.png'
-                    />
-                    <link
-                        rel='icon'
-                        type='image/png'
-                        sizes='32x32'
-                        href='/favicon-32x32.png'
-                    />
-                    <link
-                        rel='icon'
-                        type='image/png'
-                        sizes='16x16'
-                        href='/favicon-16x16.png'
-                    />
-                    <link
-                        rel='icon'
-                        type='image/png'
-                        sizes='192x192'
-                        href='/android-chrome-192x192.png'
-                    />
-                    <link
-                        rel='icon'
-                        type='image/png'
-                        sizes='512x512'
-                        href='/android-chrome-512x512.png'
-                    />
-                    <link rel='icon' type='image/png' href='/favicon.ico' />
-                    <link rel='manifest' href='/site.webmanifest' />                    
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Olubisi David - A Full stack JavaScript web developer."
+          />
+          <meta
+            name="keywords"
+            content="e-commerce,company site,cms,javascript, developer, web developer, hire web developer"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/android-chrome-512x512.png"
+          />
+          <link rel="icon" type="image/png" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
