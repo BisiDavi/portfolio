4d6d9b206defafc841fe85fada0790d829608e17
BisiDavi <oludavidconnect@gmail.com>
2023-04-26 13:15:25 +0100

    portfolio json updated

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -92,7 +92,7 @@ export default function Portfolio() {
                 flex-direction: column;
                 align-items: center;
                 justify-content: center;
-                margin-top: -50px;
+                margin-top: -55px;
                 margin-bottom: 30px;
               }
             }
